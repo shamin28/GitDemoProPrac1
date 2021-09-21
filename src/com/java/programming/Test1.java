755aeeb43f9ad7cbf7e80c45594095ec093a4379
shammilbil <shammzemichael@gmail.com>
2021-09-21 00:36:24 -0400

    First commit.....

---
+++ src/com/java/programming/Test1.java
@@ -0,0 +1,19 @@
+package com.java.programming;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String a = " I like  programming !";
+		String b = " I like  \n programming !";
+		String c = " I like \r programming !";
+		String d = " I like  \t programming !";
+
+		System.out.println("a ---> [" + a + "] ");
+		System.err.println("b ---> [" + b + "] ");
+		System.err.println("c ---> [" + c + "] ");
+		System.out.println("d ---> [" + d + "] ");
+
+	}
+
+}
